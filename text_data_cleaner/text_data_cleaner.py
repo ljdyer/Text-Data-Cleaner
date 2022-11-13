405856b85c56f459c23c90a819cb710724d8dbaf
Laurence Dyer <ljdyer@gmail.com>
2022-11-13 10:01:10 +0000

    Update text_data_cleaner.py

--- text_data_cleaner/text_data_cleaner.py
+++ text_data_cleaner/text_data_cleaner.py
@@ -24,18 +24,25 @@ PREVIEW_AFTER = """\
 class TextDataCleaner:
 
     # ====================
-    def __init__(self, docs: Union[List, pd.Series]):
+    def __init__(self,
+                 docs: Union[List, pd.Series],
+                 normalize_spaces: bool = True):
         """Initialize an instance of the class
 
         Args:
           docs (Union[List, pd.Series]):
             The list of documents to clean.
+          normalize_spaces (bool, optional):
+            Whether or not to replace two or more subsequent spaces with a single
+            space after carrying out each operation. Defaults to True.
         """
 
         if isinstance(docs, pd.Series):
-            docs = docs.to_list()    
+            docs = docs.to_list()
         self.docs_orig = docs
         self.docs_latest = docs
+        self.operation_history = []
+        self.normalize_spaces = normalize_spaces
 
     # ====================
     def show_counts(self):
@@ -84,8 +91,7 @@ class TextDataCleaner:
     def preview_replace(self,
                         find_replace: Tuple[str],
                         num_samples: int = 10,
-                        context_chars_before_after: int = 25,
-                        normalize_spaces: bool = True):
+                        context_chars_before_after: int = 25):
         """Preview the effect of carrying out a regular expression replacement
         operation on the dataset.
 
@@ -101,9 +107,6 @@ class TextDataCleaner:
           context_chars_before_after (int, optional):
             The number of characters to display before and after the replacement location in
             each sample. Defaults to 25.
-          normalize_spaces (bool, optional):
-            Whether to normalize spaces (replace two or more subsequent spaces with a single
-            space) after carryig out the replacement operation. Defaults to True.
         """
 
         find, replace = find_replace
@@ -126,12 +129,13 @@ class TextDataCleaner:
         for doc_idx, match_idx in sample_idxs:
             doc = docs[doc_idx]
             match = matches_by_doc[doc_idx][match_idx]
-            preview_before, preview_after = preview_before_and_after(
+            preview_before, preview_after = self.preview_before_and_after(
                 doc, find, replace, match, context_chars_before_after)
             samples.append(
                 {
                     'Document index': doc_idx,
-                    'Match number': f"{match_idx+1} of {len(matches_by_doc[doc_idx])}",
+                    'Match number': (f"{match_idx+1} of " +
+                                     f"{len(matches_by_doc[doc_idx])}"),
                     'Before': preview_before,
                     'After': preview_after
                 }
@@ -144,57 +148,88 @@ class TextDataCleaner:
                 )
             )
         print(
-            f'Total of {len(doc_and_match_idxs)} matches in {num_docs_with_matches}',
-            'documents (rows).'
+            f'Total of {len(doc_and_match_idxs)} matches in ' +
+            f'{num_docs_with_matches} documents (rows).'
         )
 
     # ====================
-    def regex_replace(self,
-                      regex_list: list,
-                    df: pd.DataFrame,
-                    text_column_name: str = 'Text',
-                    norm_spaces: bool = True,
-                    drop_empty_rows: bool = True
-                    ) -> pd.DataFrame:
+    def replace(self,
+                find_replace: Union[List[Tuple], Tuple]):
 
-        """Perform a sequence of one or more regex replace operations on all cells
-        in the text column of your dataframe
-
-        Required arguments:
-        -------------------
-        regex_list: list                    A list of (find, replace) tuples
-                                            e.g. (r'\(\w+\)', r'')
-        df: pd.DataFrame                    A dataframe with a text column
-
-        Optional keyword arguments:
-        ---------------------------
-        text_column_name: str = 'Text'      The name of the text column in the
-                                            dataframe
-        norm_spaces: bool = True,           If True, normalizes spaces after
-                                            performing replacement
-        drop_empty_rows: bool = True        If True, drops any rows with empty
-                                            strings following replacement
-        """
-
-        for find_re, replace_re in regex_list:
-            df[text_column_name] = df[text_column_name].apply(
-                lambda x: re.sub(find_re, replace_re, x))
-
-        # Normalize spaces
-        if norm_spaces:
-            df = normalize_spaces(df, text_column_name=text_column_name)
-
-        # Drop empty rows
-        if drop_empty_rows:
-            df[text_column_name].replace('', np.nan, inplace=True)
-            df[text_column_name].replace(' ', np.nan, inplace=True)
-            df.dropna(subset=[text_column_name], inplace=True)
-
+        if isinstance(find_replace, tuple):
+            find_replace = [find_replace]
+        for find, replace in find_replace:
+            self.docs_latest = [
+                self.re_replace(find, replace, doc) for doc in self.docs_latest
+            ]
+        # Drop empty docs
+        len_before = len(self.docs_latest)
+        self.docs_latest = [
+            doc for doc in self.docs_latest
+            if not (doc.isspace() or len(doc) == 0)
+        ]
+        len_after = len(self.docs_latest)
+        if len_after < len_before:
+            print(
+                "Removed {len_before-len_after} documents that were empty or " +
+                "contained only spaces following the operations."
+            )
         print('Done.')
-        show_doc_and_word_counts(df, text_column_name=text_column_name)
+        self.operation_history.update(find_replace)
+        print()
+        print('Latest counts:')
+        self.show_counts()
 
-        return df
+    # ====================
+    def preview_before_and_after(self,
+                                 doc: str,
+                                 find: str,
+                                 replace: str,
+                                 match: re.Match,
+                                 context_chars_before_after: int
+                                 ) -> Tuple[str]:
 
+        match_start, match_end = match.span()
+        match_str = doc[match_start:match_end]
+        text_before = doc[:match_start]
+        text_after = doc[match_end:]
+        ellipsis_before, context_before = \
+            get_context_before(text_before, context_chars_before_after)
+        ellipsis_after, context_after = \
+            get_context_after(text_after, context_chars_before_after)
+        preview_before = PREVIEW_BEFORE.format(
+            ellipsis_before=ellipsis_before,
+            text_before=html.escape(context_before),
+            match=html.escape(match_str),
+            text_after=html.escape(context_after),
+            ellipsis_after=ellipsis_after
+        )
+        text_before = self.re_replace(find, replace, text_before)
+        text_after = self.re_replace(find, replace, text_after)
+        replacement = self.re_replace(find, replace, match_str)
+        ellipsis_before, context_before = \
+            get_context_before(text_before, context_chars_before_after)
+        ellipsis_after, context_after = \
+            get_context_after(text_after, context_chars_before_after)   
+        preview_after = PREVIEW_AFTER.format(
+            ellipsis_before=ellipsis_before,
+            text_before=html.escape(context_before),
+            replacement=html.escape(replacement),
+            text_after=html.escape(context_after),
+            ellipsis_after=ellipsis_after
+        )
+        return preview_before, preview_after
+
+    # ====================
+    def re_replace(self,
+                   find: str,
+                   replace: str,
+                   text: str) -> str:
+
+        replaced = re.sub(find, replace, text)
+        if self.normalize_spaces:
+            replaced = re.sub(r'  +', ' ', replaced)
+        return replaced
 
 # # ====================
 # def normalize_unicode(df: pd.DataFrame,
@@ -221,43 +256,6 @@ class TextDataCleaner:
 
 # # === HELPER FUNCTIONS ===
 
-# ====================
-def preview_before_and_after(doc: str,
-                             find: str,
-                             replace: str,
-                             match: re.Match,
-                             context_chars_before_after: int) -> Tuple[str]:
-
-    match_start, match_end = match.span()
-    match_str = doc[match_start:match_end]
-    text_before = doc[:match_start]
-    text_after = doc[match_end:]
-    ellipsis_before, context_before = \
-        get_context_before(text_before, context_chars_before_after)
-    ellipsis_after, context_after = \
-        get_context_after(text_after, context_chars_before_after)
-    preview_before = PREVIEW_BEFORE.format(
-        ellipsis_before=ellipsis_before,
-        text_before=html.escape(context_before),
-        match=html.escape(match_str),
-        text_after=html.escape(context_after),
-        ellipsis_after=ellipsis_after
-    )
-    text_before = re.sub(find, replace, text_before)
-    text_after = re.sub(find, replace, text_after)
-    replacement = re.sub(find, replace, match_str)
-    ellipsis_before, context_before = \
-        get_context_before(text_before, context_chars_before_after)
-    ellipsis_after, context_after = \
-        get_context_after(text_after, context_chars_before_after)   
-    preview_after = PREVIEW_AFTER.format(
-        ellipsis_before=ellipsis_before,
-        text_before=html.escape(context_before),
-        replacement=html.escape(replacement),
-        text_after=html.escape(context_after),
-        ellipsis_after=ellipsis_after
-    )
-    return preview_before, preview_after
 
 
 # ====================
@@ -276,25 +274,6 @@ def get_context_after(text: str,
     ellipsis = ' ...' if len(text) > context_len else '    '
     context = text[:context_len].ljust(context_len)
     return ellipsis, context
-
-
-# ====================
-def normalize_spaces(df: pd.DataFrame,
-                     text_column_name='Text'
-                     ) -> pd.DataFrame:
-
-    """Normalize spaces in all cells in text column of a dataframe"""
-
-    df[text_column_name] = df[text_column_name].apply(normalize_spaces_string)
-    return df
-
-
-# ====================
-def normalize_spaces_string(input_str: str) -> str:
-
-    """Normalize spaces in a string"""
-
-    return re.sub('  +', ' ', input_str)
 
 
 # ====================
